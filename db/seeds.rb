2516b89800e5ac159a568213a9005085726363aa
fentyhall <fentymhall@gmail.com>
2021-02-24 07:26:42 -0800

    category model migration and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+architecture = Category.create(name: "Architecture")
+sports = Category.create(name: "Sports")
+science = Category.create(name: "Science")
