c31f9ed5d9871c19a3211078a6382e5619668c20
fentyhall <fentymhall@gmail.com>
2021-02-27 17:07:31 -0800

    initiate adapters for api request

--- app/adapters/api.rb
+++ app/adapters/api.rb
@@ -1,4 +1,6 @@
 class Api
+
+    TRIVIA = {}
     
     # API from https://opentdb.com/api.php?amount=50&category=27&type=multiple
     def self.animals
@@ -7,12 +9,29 @@ class Api
         response = Net::HTTP.get_response(uri)
         
         data = JSON.parse(response.body)
-
-        binding.pry
-        animals_array = []
         
+        animals = []
 
+        data["results"][0].each do |data_obj|
+            animal = {}
 
-        File.write("animals.json", animals_array.to_json)
+            animal[:question] = data_obj["question"]
+
+            # Shuffling multiple choice
+            multiple_choice = [data["results"][0]["correct_answer"], data["results"][0]["incorrect_answers"][0], data["results"][0]["incorrect_answers"][1], data["results"][0]["incorrect_answers"][2]].shuffle
+
+            animal[:choice1] = multiple_choice[0]
+            animal[:choice2] = multiple_choice[1]
+            animal[:choice3] = multiple_choice[2]
+            animal[:choice4] = multiple_choice[3]
+            
+            animal[:answer] = data_obj["correct_answer"]
+
+            animals << animal
+        end 
+
+        TRIVIA << animals
     end 
+
+    File.write("trivia.json", TRIVIA.to_json)
 end 
