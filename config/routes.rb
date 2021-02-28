504982b4e803b5818ac831f34bcd2da6e1562031
fentyhall <fentymhall@gmail.com>
2021-02-28 13:42:35 -0800

    add routes and categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do
-      resources :categories, only: [:index, :show]
+      resources :categories, only: [:index] do 
+        resources :questions, only: [:index]
+      end 
     end 
   end 
 end
