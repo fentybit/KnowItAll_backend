822fc91fcd864526e5695fb4556f74ed0b3feaaf
fentyhall <fentymhall@gmail.com>
2021-02-27 17:17:45 -0800

    finalize adapters api for animals json API

--- app/adapters/api.rb
+++ app/adapters/api.rb
@@ -1,6 +1,4 @@
 class Api
-
-    TRIVIA = {}
     
     # API from https://opentdb.com/api.php?amount=50&category=27&type=multiple
     def self.animals
@@ -12,26 +10,25 @@ class Api
         
         animals = []
 
-        data["results"][0].each do |data_obj|
+        data["results"].each do |data_obj|
             animal = {}
 
-            animal[:question] = data_obj["question"]
+            animal["question"] = data_obj["question"]
 
             # Shuffling multiple choice
             multiple_choice = [data["results"][0]["correct_answer"], data["results"][0]["incorrect_answers"][0], data["results"][0]["incorrect_answers"][1], data["results"][0]["incorrect_answers"][2]].shuffle
 
-            animal[:choice1] = multiple_choice[0]
-            animal[:choice2] = multiple_choice[1]
-            animal[:choice3] = multiple_choice[2]
-            animal[:choice4] = multiple_choice[3]
+            animal["choice1"] = multiple_choice[0]
+            animal["choice2"] = multiple_choice[1]
+            animal["choice3"] = multiple_choice[2]
+            animal["choice4"] = multiple_choice[3]
             
-            animal[:answer] = data_obj["correct_answer"]
+            animal["answer"] = data_obj["correct_answer"]
 
             animals << animal
         end 
 
-        TRIVIA << animals
+        File.write("animals.json", animals.to_json)
     end 
 
-    File.write("trivia.json", TRIVIA.to_json)
 end 
