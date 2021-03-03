a0ed34af3c7cb6a13e1c37823c75481ea3f6472c
fentyhall <fentymhall@gmail.com>
2021-03-03 13:02:10 -0800

    re-factor Question Serializer

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,4 +1,4 @@
 class QuestionSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :question, :choice1, :choice2, :choice3, :choice4, :answer, :category_id, :category
+  attributes :question, :choice1, :choice2, :choice3, :choice4, :answer, :category_id, :category_name
 end
