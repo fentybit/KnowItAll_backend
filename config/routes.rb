c9ca55008a626ba086221e52409382ce21c95591
fentyhall <fentymhall@gmail.com>
2021-03-07 15:17:28 -0800

    add update method to config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do 
-		resources :users, only: [:index, :create, :show]
+		resources :users, only: [:index, :create, :show, :update]
 	end 
 
   namespace :api do 
